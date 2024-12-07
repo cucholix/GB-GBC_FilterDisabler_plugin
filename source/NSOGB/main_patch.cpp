10b9c421a9bac31b05330c694e849a3af4e4d5dc
cucholix <cucholix@gmail.com>
2024-12-07 13:38:39 -0300

    Update main_patch.cpp

--- source/NSOGB/main_patch.cpp
+++ source/NSOGB/main_patch.cpp
@@ -2,18 +2,17 @@
 #include "skyline/utils/cpputils.hpp"
 #include "skyline/inlinehook/memcpy_controlled.hpp"
 
-/*08 51 40 F8 56 03 80 52 E1 63 00 91 E0 03 15 AA 
-  F6 63 00 39 FF 9B 00 39 E9 93 01 F8 E8 E3 01 F8*/
-constinit uint8_t nostalgic[32] = {0x08, 0x51, 0x40, 0xF8, 0x56, 0x03, 0x80, 0x52, 0xE1, 0x63, 0x00, 0x91, 0xE0, 0x03, 0x15, 0xAA, 
-                                   0xF6, 0x63, 0x00, 0x39, 0xFF, 0x9B, 0x00, 0x39, 0xE9, 0x93, 0x01, 0xF8, 0xE8, 0xE3, 0x01, 0xF8};
+/*A0 63 00 D1 81 00 80 52 - L_CLASSICS_FILTER_PRESET_CGB_DEFAULT=4*/
+constinit uint8_t CGB_DEFAULT[8] = {0xA0, 0x63, 0x00, 0xD1, 0x81, 0x00, 0x80, 0x52};
 								   
-/*08 51 40 F8 56 03 80 52 E1 63 00 91 E0 03 15 AA 
-  F6 63 00 39 FF 9B 00 39 E9 93 01 F8 E8 E3 01 F8
-  1F 20 03 D5 | (nop sub_sub_710000XXXX)*/  
-constinit uint8_t nostalgic_p[36] = {0x08, 0x51, 0x40, 0xF8, 0x56, 0x03, 0x80, 0x52, 0xE1, 0x63, 0x00, 0x91, 0xE0, 0x03, 0x15, 0xAA, 
-                                     0xF6, 0x63, 0x00, 0x39, 0xFF, 0x9B, 0x00, 0x39, 0xE9, 0x93, 0x01, 0xF8, 0xE8, 0xE3, 0x01, 0xF8,
-								     0x1F, 0x20, 0x03, 0xD5};
+/*A0 63 00 D1 41 00 80 52 - L_CLASSICS_FILTER_PRESET_CGB_DEFAULT=2*/
+constinit uint8_t CGB_DEFAULT_p[8] = {0xA0, 0x63, 0x00, 0xD1, 0x41, 0x00, 0x80, 0x52};
 
+/*B5 00 80 52 A0 63 00 D1 - L_CLASSICS_FILTER_PRESET_CGB_NOSTALGIC=5*/
+constinit uint8_t CGB_NOSTALGIC[8] = {0xB5, 0x00, 0x80, 0x52, 0xA0, 0x63, 0x00, 0xD1};
+								   
+/*75 00 80 52 A0 63 00 D1 - L_CLASSICS_FILTER_PRESET_CGB_NOSTALGIC=3*/
+constinit uint8_t CGB_NOSTALGIC_p[8] = {0x75, 0x00, 0x80, 0x52, 0xA0, 0x63, 0x00, 0xD1};
 
 bool memcmp_f (const unsigned char *s1, const unsigned char *s2, size_t count) {
     while (count-- > 0)
@@ -43,8 +42,12 @@ uintptr_t findTextCode(const uint8_t* code, size_t size) {
 }
 
 void NSOGB_filter_preset_main() {	
-    uintptr_t pointer = findTextCode(&nostalgic[0], sizeof(nostalgic));
+    uintptr_t pointer = findTextCode(&CGB_DEFAULT[0], sizeof(CGB_DEFAULT));
     if (pointer) {
-        sky_memcpy((void*)pointer, &nostalgic_p[0], sizeof(nostalgic_p));
+        sky_memcpy((void*)pointer, &CGB_DEFAULT_p[0], sizeof(CGB_DEFAULT_p));
+    }
+	pointer = findTextCode(&CGB_NOSTALGIC[0], sizeof(CGB_NOSTALGIC));
+    if (pointer) {
+        sky_memcpy((void*)pointer, &CGB_NOSTALGIC_p[0], sizeof(CGB_NOSTALGIC_p));
     }
 }
